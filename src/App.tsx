e6ce4152fd11ee52b2f6058863ce37d814574b75
seo-wo <4nyth4me@gmail.com>
2023-11-21 20:29:41 +0900

    fix: change for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import GoogleLogin from './pages/GoogleLogin';
+import LoginCallback from './pages/LoginCallback';
+import AuthTest from './pages/AuthTest';
+import Home from './pages/Home';
 
+declare global {
+  interface Window {
+    googleLoginPopup: Window | null;
+  }
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<GoogleLogin />} />
+      <Route path="/login/callback/:token" element={<LoginCallback />} />
+      <Route path="/test" element={<AuthTest />} />
+    </Routes>
+  )
 }
-
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React, { useCallback, useState, useEffect } from 'react';
+
+export default function Home() {
+  useEffect(() => {;
+  }, []);
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>test</p>
+    </div>
+  )
+}
